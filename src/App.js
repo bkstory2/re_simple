7d996bc7fbe1fa8bb8a36900be456b8ed9790e04
bkstory2 <bkstory@nate.com>
2025-05-25 21:33:23 +0900

    색 적용 해기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink, Navigate } from 'react-router-dom';
 
 import DbTodo from './board/DbTodo';
 import TxtTodo from './board/TxtTodo';
 
 function App() {
+  const activeStyle = {
+    color: 'red',
+    fontWeight: 'bold',
+  };
+
   return (
     <Router>
       <div className="App">
         <nav style={{ marginBottom: '20px' }}>
-          <Link to="/board/DbTodo" style={{ marginRight: '10px' }}>DB방식</Link>
-          <Link to="/board/TxtTodo">메모리방식</Link>
+          <NavLink
+            to="/board/DbTodo"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            DB방식
+          </NavLink>
+          <span style={{ margin: '0 10px' }}></span>
+          <NavLink
+            to="/board/TxtTodo"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            메모리방식
+          </NavLink>
         </nav>
 
-        
-
         <Routes>
-          {/* ✅ 루트("/") 접근 시 /board/txt 로 리디렉션 */}
           <Route path="/" element={<Navigate to="/board/DbTodo" replace />} />
-          
           <Route path="/board/DbTodo" element={<DbTodo />} />
           <Route path="/board/TxtTodo" element={<TxtTodo />} />
         </Routes>
